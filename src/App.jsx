93aab9a754020d5582d22ac0d07bce0f32c95614
Abin John <abinjohnanto@gmail.com>
2023-07-01 19:41:37 +0530

    single recipe page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from 'react-router-dom';
 
 import './App.scss';
 import Home from './components/Home';
+import Recipe from './components/Recipe';
 
 function App() {
   return (
     <>
-      <h1>Organico</h1>
+      <h1>Organi.co</h1>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/recipe/:recipeId" element={<Recipe />} />
       </Routes>
     </>
   );
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,5 +1,39 @@
 import { useParams } from 'react-router';
 
+import { useAppContext } from '../AppContext';
+
 const Recipe = () => {
-  const { id } = useParams();
+  const { recipeId } = useParams();
+  const { recipes } = useAppContext();
+
+  const singleRecipe = recipes.find(({ id }) => id === recipeId);
+
+  const { name, cuisine, ingredients, steps, imageURL } = singleRecipe;
+
+  return (
+    <div className="single-recipe-card">
+      <div className="img-container">
+        <img src={imageURL} alt={name} />
+      </div>
+      <div className="details-container">
+        <h2>{name}</h2>
+        <p className="cuisine">Cuisine: {cuisine}</p>
+
+        <p>
+          <strong>Ingredients:</strong>{' '}
+          {ingredients
+            .map(({ name, quantity }) => `${name} x ${quantity}`)
+            .join(', ')}
+        </p>
+        <ul className="steps">
+          <strong>Steps</strong>
+          {steps.map((step, key) => (
+            <li key={key}>{step}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
+
+export default Recipe;
